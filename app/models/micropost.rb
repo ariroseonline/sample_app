f5d9f437306710e91fb74f9ec0f7c4862720256f
Ari Rose <ariroseonline@gmail.com>
2012-01-14 14:52:50 -0500

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,11 @@ class Micropost < ActiveRecord::Base
   
   belongs_to :user
   
+  validates :content, :presence => true, :length => { :minimum => 140}
+  validates :user_id, :presence => true
+  
   default_scope :order => "microposts created_at DESC"
+
 end
 # == Schema Information
 #
